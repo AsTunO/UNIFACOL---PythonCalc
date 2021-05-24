136e383f2e9f4ac7c61aecb1b5f45d7dfb895cda
Júlio Cesar <juliocesar27072004@outlook.com>
2021-05-24 14:04:31 -0300

    Arquivo .ui convertido para .py

---
+++ src/WinGUI.py
@@ -0,0 +1,223 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'style.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+
+
+class Ui_.(object):
+    def setupUi(self, _):
+        if not _.objectName():
+            _.setObjectName(u"_")
+        _.resize(215, 275)
+        _.setStyleSheet(u"background-color: rgb(18, 138, 138);\n"
+"")
+        self.centralwidget = QWidget(_)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.pushButton = QPushButton(self.centralwidget)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setGeometry(QRect(10, 110, 31, 23))
+        font = QFont()
+        font.setFamily(u"Arial")
+        font.setBold(True)
+        font.setWeight(75)
+        self.pushButton.setFont(font)
+        self.pushButton.setStyleSheet(u"color: rgb(255, 255, 255);\n"
+"")
+        self.pushButton_2 = QPushButton(self.centralwidget)
+        self.pushButton_2.setObjectName(u"pushButton_2")
+        self.pushButton_2.setGeometry(QRect(90, 110, 31, 23))
+        self.pushButton_2.setFont(font)
+        self.pushButton_2.setStyleSheet(u"color: rgb(255, 255, 255);\n"
+"")
+        self.pushButton_3 = QPushButton(self.centralwidget)
+        self.pushButton_3.setObjectName(u"pushButton_3")
+        self.pushButton_3.setGeometry(QRect(50, 110, 31, 23))
+        self.pushButton_3.setFont(font)
+        self.pushButton_3.setStyleSheet(u"color: rgb(255, 255, 255);\n"
+"")
+        self.pushButton_4 = QPushButton(self.centralwidget)
+        self.pushButton_4.setObjectName(u"pushButton_4")
+        self.pushButton_4.setGeometry(QRect(10, 200, 31, 23))
+        self.pushButton_4.setFont(font)
+        self.pushButton_4.setStyleSheet(u"color: rgb(255, 255, 255);\n"
+"")
+        self.pushButton_5 = QPushButton(self.centralwidget)
+        self.pushButton_5.setObjectName(u"pushButton_5")
+        self.pushButton_5.setGeometry(QRect(90, 140, 31, 23))
+        self.pushButton_5.setFont(font)
+        self.pushButton_5.setStyleSheet(u"color: rgb(255, 255, 255);\n"
+"")
+        self.pushButton_6 = QPushButton(self.centralwidget)
+        self.pushButton_6.setObjectName(u"pushButton_6")
+        self.pushButton_6.setGeometry(QRect(90, 170, 31, 23))
+        self.pushButton_6.setFont(font)
+        self.pushButton_6.setStyleSheet(u"color: rgb(255, 255, 255);\n"
+"")
+        self.pushButton_7 = QPushButton(self.centralwidget)
+        self.pushButton_7.setObjectName(u"pushButton_7")
+        self.pushButton_7.setGeometry(QRect(50, 170, 31, 23))
+        self.pushButton_7.setFont(font)
+        self.pushButton_7.setStyleSheet(u"color: rgb(255, 255, 255);\n"
+"")
+        self.pushButton_8 = QPushButton(self.centralwidget)
+        self.pushButton_8.setObjectName(u"pushButton_8")
+        self.pushButton_8.setGeometry(QRect(10, 170, 31, 23))
+        self.pushButton_8.setFont(font)
+        self.pushButton_8.setStyleSheet(u"color: rgb(255, 255, 255);\n"
+"")
+        self.pushButton_9 = QPushButton(self.centralwidget)
+        self.pushButton_9.setObjectName(u"pushButton_9")
+        self.pushButton_9.setGeometry(QRect(50, 140, 31, 23))
+        self.pushButton_9.setFont(font)
+        self.pushButton_9.setStyleSheet(u"color: rgb(255, 255, 255);\n"
+"")
+        self.pushButton_10 = QPushButton(self.centralwidget)
+        self.pushButton_10.setObjectName(u"pushButton_10")
+        self.pushButton_10.setGeometry(QRect(10, 140, 31, 21))
+        self.pushButton_10.setFont(font)
+        self.pushButton_10.setStyleSheet(u"color: rgb(255, 255, 255);\n"
+"")
+        self.pushButton_11 = QPushButton(self.centralwidget)
+        self.pushButton_11.setObjectName(u"pushButton_11")
+        self.pushButton_11.setGeometry(QRect(10, 230, 191, 23))
+        font1 = QFont()
+        font1.setFamily(u"Arial")
+        font1.setPointSize(10)
+        font1.setBold(True)
+        font1.setWeight(75)
+        self.pushButton_11.setFont(font1)
+        self.pushButton_11.setStyleSheet(u"color: rgb(255, 255, 255);\n"
+"background-color: rgb(0, 0, 0);\n"
+"")
+        self.pushButton_19 = QPushButton(self.centralwidget)
+        self.pushButton_19.setObjectName(u"pushButton_19")
+        self.pushButton_19.setGeometry(QRect(50, 200, 31, 23))
+        self.pushButton_19.setFont(font1)
+        self.pushButton_19.setStyleSheet(u"color: rgb(255, 255, 255);\n"
+"")
+        self.pushButton_23 = QPushButton(self.centralwidget)
+        self.pushButton_23.setObjectName(u"pushButton_23")
+        self.pushButton_23.setGeometry(QRect(170, 140, 31, 23))
+        self.pushButton_23.setFont(font1)
+        self.pushButton_23.setStyleSheet(u"color: rgb(255, 255, 255);\n"
+"")
+        self.pushButton_25 = QPushButton(self.centralwidget)
+        self.pushButton_25.setObjectName(u"pushButton_25")
+        self.pushButton_25.setGeometry(QRect(130, 200, 31, 23))
+        self.pushButton_25.setFont(font1)
+        self.pushButton_25.setStyleSheet(u"color: rgb(255, 255, 255);\n"
+"")
+        self.pushButton_26 = QPushButton(self.centralwidget)
+        self.pushButton_26.setObjectName(u"pushButton_26")
+        self.pushButton_26.setGeometry(QRect(130, 170, 31, 23))
+        self.pushButton_26.setFont(font1)
+        self.pushButton_26.setStyleSheet(u"color: rgb(255, 255, 255);\n"
+"")
+        self.pushButton_27 = QPushButton(self.centralwidget)
+        self.pushButton_27.setObjectName(u"pushButton_27")
+        self.pushButton_27.setGeometry(QRect(170, 170, 31, 23))
+        self.pushButton_27.setFont(font1)
+        self.pushButton_27.setStyleSheet(u"color: rgb(255, 255, 255);\n"
+"")
+        self.pushButton_28 = QPushButton(self.centralwidget)
+        self.pushButton_28.setObjectName(u"pushButton_28")
+        self.pushButton_28.setGeometry(QRect(170, 200, 31, 23))
+        self.pushButton_28.setFont(font1)
+        self.pushButton_28.setStyleSheet(u"color: rgb(255, 255, 255);\n"
+"")
+        self.pushButton_20 = QPushButton(self.centralwidget)
+        self.pushButton_20.setObjectName(u"pushButton_20")
+        self.pushButton_20.setGeometry(QRect(130, 110, 31, 23))
+        self.pushButton_20.setFont(font1)
+        self.pushButton_20.setStyleSheet(u"color: rgb(255, 255, 255);\n"
+"")
+        self.pushButton_21 = QPushButton(self.centralwidget)
+        self.pushButton_21.setObjectName(u"pushButton_21")
+        self.pushButton_21.setGeometry(QRect(130, 140, 31, 23))
+        self.pushButton_21.setFont(font1)
+        self.pushButton_21.setStyleSheet(u"color: rgb(255, 255, 255);\n"
+"")
+        self.pushButton_22 = QPushButton(self.centralwidget)
+        self.pushButton_22.setObjectName(u"pushButton_22")
+        self.pushButton_22.setGeometry(QRect(90, 200, 31, 23))
+        self.pushButton_22.setFont(font1)
+        self.pushButton_22.setStyleSheet(u"color: rgb(255, 255, 255);\n"
+"")
+        self.pushButton_32 = QPushButton(self.centralwidget)
+        self.pushButton_32.setObjectName(u"pushButton_32")
+        self.pushButton_32.setGeometry(QRect(170, 110, 31, 23))
+        self.pushButton_32.setFont(font)
+        self.pushButton_32.setStyleSheet(u"color: rgb(255, 255, 255);\n"
+"\n"
+"")
+        self.label = QLabel(self.centralwidget)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(10, 10, 191, 31))
+        font2 = QFont()
+        font2.setFamily(u"Arial")
+        font2.setPointSize(14)
+        font2.setBold(False)
+        font2.setWeight(50)
+        self.label.setFont(font2)
+        self.label.setStyleSheet(u"background-color: rgb(255, 255, 255);\n"
+"color: rgb(0,0,0);\n"
+"border-radius: 15px;")
+        self.label.setAlignment(Qt.AlignCenter)
+        self.label.setMargin(0)
+        self.label_3 = QLabel(self.centralwidget)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setGeometry(QRect(10, 50, 191, 41))
+        font3 = QFont()
+        font3.setPointSize(20)
+        font3.setBold(True)
+        font3.setWeight(75)
+        self.label_3.setFont(font3)
+        self.label_3.setStyleSheet(u"background-color: rgb(255, 255, 255);\n"
+"border-radius: 15px;\n"
+"padding: 5px;")
+        _.setCentralWidget(self.centralwidget)
+        self.statusbar = QStatusBar(_)
+        self.statusbar.setObjectName(u"statusbar")
+        _.setStatusBar(self.statusbar)
+
+        self.retranslateUi(_)
+
+        QMetaObject.connectSlotsByName(_)
+    # setupUi
+
+    def retranslateUi(self, _):
+        _.setWindowTitle(QCoreApplication.translate(".", u"MainWindow", None))
+        self.pushButton.setText(QCoreApplication.translate(".", u"1", None))
+        self.pushButton_2.setText(QCoreApplication.translate(".", u"3", None))
+        self.pushButton_3.setText(QCoreApplication.translate(".", u"2", None))
+        self.pushButton_4.setText(QCoreApplication.translate(".", u"0", None))
+        self.pushButton_5.setText(QCoreApplication.translate(".", u"6", None))
+        self.pushButton_6.setText(QCoreApplication.translate(".", u"9", None))
+        self.pushButton_7.setText(QCoreApplication.translate(".", u"8", None))
+        self.pushButton_8.setText(QCoreApplication.translate(".", u"7", None))
+        self.pushButton_9.setText(QCoreApplication.translate(".", u"5", None))
+        self.pushButton_10.setText(QCoreApplication.translate(".", u"4", None))
+        self.pushButton_11.setText(QCoreApplication.translate(".", u"=", None))
+        self.pushButton_19.setText(QCoreApplication.translate(".", u".", None))
+        self.pushButton_23.setText(QCoreApplication.translate(".", u"%", None))
+        self.pushButton_25.setText(QCoreApplication.translate(".", u"*", None))
+        self.pushButton_26.setText(QCoreApplication.translate(".", u"/", None))
+        self.pushButton_27.setText(QCoreApplication.translate(".", u"//", None))
+        self.pushButton_28.setText(QCoreApplication.translate(".", u"**", None))
+        self.pushButton_20.setText(QCoreApplication.translate(".", u"+", None))
+        self.pushButton_21.setText(QCoreApplication.translate(".", u"-", None))
+        self.pushButton_22.setText(QCoreApplication.translate(".", u"\u221a", None))
+        self.pushButton_32.setText(QCoreApplication.translate(".", u"C", None))
+        self.label.setText(QCoreApplication.translate(".", u"Calculadora", None))
+        self.label_3.setText(QCoreApplication.translate(".", u"0", None))
+    # retranslateUi
+
